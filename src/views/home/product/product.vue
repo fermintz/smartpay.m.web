7ec97d1b6f27bba2735e42f95e434409f19edc0b
fermintz <fermintz@gmail.com>
2022-07-12 18:12:54 +0900

    상품선택_장비선택추가

--- src/views/home/product/product.vue
+++ src/views/home/product/product.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="product">
     <Header />
-    <div class="title">
-      <span>선택됨</span>
+    <div class="title"> 
+      <span>선택된 장비</span>
       <h5 @click="alert">
-        <label>테스트 장비(사무실 조수원자리리리리리리)</label>
+        <label>테스트 장비</label>
         <q-icon name="arrow_drop_down_circle"></q-icon>
       </h5>
       <p>장비를 다시 선택하시려면 <b>QR코드</b>를 다시 스캔해주세요</p>
